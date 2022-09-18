84b16f48201eddd35a164e348b9dbd7f4963f78f
miki-matsumoto <mikimatsumoto829@gmail.com>
2022-09-18 14:35:55 +0900

    install next auth

---
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+console.log(process.env.GOOGLE_CLIENT_ID);
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    //jwtが作成・更新された時に呼ばれる
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    //セッションがチェックされた時に呼ばれる
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  secret: "hey",
+});
--- apps/web/src/pages/api/hello.ts
+++ apps/web/src/pages/api/hello.ts
@@ -11,6 +11,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log(initTRPC);
+  console.log(process.env.GOOGLE_CLIENT_ID);
   res.status(200).json({ name: "John Doe", initTRPC });
 }
